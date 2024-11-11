7f1f1615a517a3fe66271d25c8c27d79ec75e5df
Jason Offet <joffet@venue.live>
2024-11-10 21:08:34 -0500

    first commmit

---
+++ README.md
@@ -0,0 +1,12 @@
+Seconda Take Home Assignment - Jason Offet
+Node Middleware
+This is required to avoid CORS conflicts
+
+Have this up and running prior to running the React app
+
+Instructions:
+
+1. (clone the repo)
+2. run npm install
+3. node app.js
+4. This should be running at http://localhost:4000/
---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 4000;
+
+const getCoinData = async () => {
+  let response = null;
+  try {
+    response = await fetch(
+      "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=10&sort=market_cap&cryptocurrency_type=all&tag=all",
+      {
+        method: "GET",
+        headers: {
+          "X-CMC_PRO_API_KEY": 
+        },
+      }
+    );
+  } catch (error) {
+    console.log({ error });
+  }
+  console.log(response);
+  return response;
+};
+
+app.listen(PORT, () => {
+  console.log("Server Listening on PORT:", PORT);
+});
+
+app.get("/get", async (request, response) => {
+  const data = await getCoinData();
+  const json = await data?.json();
+  response.set("Access-Control-Allow-Origin", "*"); // CORS sidestep
+  response.send(json);
+});
